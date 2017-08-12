510c7d13020c6a1996fc8969a0863f890f3c89d3
jacobfriis <jacob.friis@simplecomplex.net>
2017-08-12 13:33:50 +0200

    crediting

--- src/HttpRequest.php
+++ src/HttpRequest.php
@@ -2,6 +2,7 @@
 /**
  * KIT/Koncernservice, Københavns Kommune.
  * @link https://kkgit.kk.dk/php-psr.kk-seb/http
+ * @author Jacob Friis Mathiasen <jacob.friis.mathiasen@ks.kk.dk>
  */
 declare(strict_types=1);
 
