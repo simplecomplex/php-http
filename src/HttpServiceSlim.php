67b7e82c27ed763ceb512decf754fec7b802371f
jacobfriis <jacob.friis@simplecomplex.net>
2017-08-17 19:42:44 +0200

    HttpServiceSlim method to append CORS exposable headers.

--- src/HttpServiceSlim.php
+++ src/HttpServiceSlim.php
@@ -18,7 +18,7 @@ use Slim\Http\Response;
 class HttpServiceSlim
 {
     /**
-     * Add Cross Origin headers, preferably only in development.
+     * Set Cross Origin headers, preferably only in development.
      *
      * Circumvent CORS (cross origin resource sharing) check in development.
      * Angular serves from other host in development;
@@ -29,11 +29,11 @@ class HttpServiceSlim
      * - vendor/kk-seb/http/config-ini/http.prod.override.ini
      *
      * @param \Slim\Http\Response $response
-     * @param array|null $allowedSites
+     * @param array $allowedSites
      *
      * @return \Slim\Http\Response
      */
-    public static function addCrossOriginHeaders(Response $response, $allowedSites) : Response
+    public static function setCrossOriginHeaders(Response $response, array $allowedSites) : Response
     {
         if ($allowedSites) {
             // Allow requestor to see all relevant response headers;
@@ -45,6 +45,33 @@ class HttpServiceSlim
             )
                 // Allow CORS for sites.
                 ->withHeader('Access-Control-Allow-Origin', join(',', $allowedSites));
+        }
+        return $response;
+    }
+
+    /**
+     * Append headers to Cross Origin exposable headers header.
+     *
+     * @param \Slim\Http\Response $response
+     * @param array $headers
+     *
+     * @return Response
+     */
+    public static function appendCrossOriginExposedHeaders(Response $response, array $headers) : Response {
+        if ($headers && $response->hasHeader('Access-Control-Expose-Headers')) {
+            // withHeader() overrides previously set headers by that name.
+            $response = $response->withHeader(
+                'Access-Control-Expose-Headers',
+                join(
+                    ',',
+                    array_unique(
+                        array_merge(
+                            explode(',', $response->getHeader('Access-Control-Expose-Headers')[0]),
+                            array_keys($headers)
+                        )
+                    )
+                )
+            );
         }
         return $response;
     }
